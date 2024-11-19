720c78db966f0b754f6765bff4d421dd6529bff7
K-Town182 <kathrynncraig@student.purdueglobal.edu>
2024-11-19 13:53:33 -0600

    Added the Payment Info Page to Menu
    - Created PaymentInfoPage.xaml
    - updated AppShell.xaml

---
+++ BillFolio/PaymentInfoPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillFolio
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PaymentInfoPage : ContentPage
+	{
+		public PaymentInfoPage ()
+		{
+		//	InitializeComponent ();
+		}
+
+			//********* Had to comment this out to commit without error. I think there is an error with the InitializeComponent.
+			//********** if we can figure out that error I think the rest will work. This is the same error I've been getting for days.
+			//********** Kaytee T.
+		}
+	}
