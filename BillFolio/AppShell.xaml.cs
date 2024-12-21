52dd7f75c8b7d25e8e0fe5e10feb957f737cc326
JohnnyMaine-8492 <johnnymaine849221@gmail.com>
2024-12-20 19:56:12 -0600

    Update AppShell.xaml.cs

--- BillFolio/AppShell.xaml.cs
+++ BillFolio/AppShell.xaml.cs
@@ -1,10 +1,24 @@
-﻿namespace BillFolio
+﻿using Microsoft.Maui.Controls;
+
+namespace BillFolio
 {
-	public partial class AppShell : Shell
-	{
-		public AppShell()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute("TwoWeekView", typeof(TwoWeekView));
+        }
+
+        protected override void OnNavigated(ShellNavigatedEventArgs args)
+        {
+            base.OnNavigated(args);
+
+            // Redirect to TwoWeekView on startup
+            if (args.Current.Location.OriginalString == "//")
+            {
+                Shell.Current.GoToAsync("//TwoWeekView");
+            }
+        }
+    }
 }
